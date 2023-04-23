2e28e3c98f5b30cf98a6abe4a319fa24202f52a8
MohaFadl <Mohamed.medhat.fadl@gmail.com>
2023-04-23 03:57:24 +0200

    -i added a comment by Medhat

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+// this is an update from Medhat's Branch
 
   // This widget is the root of your application.
   @override
